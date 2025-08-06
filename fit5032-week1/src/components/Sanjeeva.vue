60393bb0e41f1bd5da0445cd2163773e82210769
Sanjeeva Reddy Vaddi <svad0009@student.monash.edu>
2025-08-06 20:44:24 +1000

    just pushing all the weekly tasks

---
+++ fit5032-week1/src/components/Sanjeeva.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="content">
+    <p>Welcome to the Sanjeeva Vue application!</p>
+    <p>This is a simple example of a Vue.js application structure.</p>
+  </div>
+</template>
